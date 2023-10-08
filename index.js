82de83dcfe3dc3de1f74de1e336aa7350eecb7d8
Lucy Nwokoye <142795917+BabyLucyJoan@users.noreply.github.com>
2023-10-08 13:10:49 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,89 @@
+//Objects
+
+const firstStudent = {
+    firstName:"John",
+    lastName :"Solomon",
+    phoneNumber :070123456,
+    address :"Ikorodu",
+    age :18,
+    score : 91,
+    gender :"Male"
+
+}
+console.log(firstStudent)
+
+
+const secondStudent= {
+    firstName:"Ike",
+    lastName: "Amaka",
+    phoneNumber: 070123457,
+    address: "Eleme",
+    age : 16,
+    score : 86,
+    gender : "Female"
+
+}
+console.log(secondStudent)
+
+
+const thirdStudent= {
+    firstName:"Johnson",
+    lastName: "Ben",
+    phoneNumber: 070123458,
+    address: "Bayelsa",
+    age : 17,
+    score : 69,
+    gender : "Male"
+
+}
+console.log(thirdStudent)
+
+
+const fourthStudent={
+    firstName:"Prince",
+    lastName: "Esther",
+    phoneNumber: 070123459,
+    address: "Gwarinpa",
+    age : 19,
+    score : 72,
+    gender : "Female"
+
+}
+console.log(thirdStudent)
+
+
+const fifthStudent={
+    firstName:"Monday",
+    lastName: "Kelvin",
+    phoneNumber: 070123450,
+    address: "Uyo",
+    age : 16,
+    score : 89,
+    gender : "Male",
+
+}
+console.log(fifthStudent);
+
+
+
+//Arrays
+
+const fruits =[ 
+    "Pawpaw", "Orange", "Guava", "Banana", "Cashew", "Strawberry", "Cherry", "Pineapple", "Apple", "Blackberry", "Pear", "Cucumber", "Watermelon", "Coconut", "Breadfruit"
+]
+
+console.log(fruits)
+
+
+
+//If/Else statements
+
+let score = 87
+
+if (score<49) {console.log("Failed")}
+else if (score>49<59) {console.log("Good")}
+else if (score>59<69) {console.log("Very good")}
+else (score>69) ;{console.log("Excellent")}
+
+
+
